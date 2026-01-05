8f149c248dd5cf7a645e9c42dfead4bb73a6b519
Ruthwik Hosadurga Madhusudhana <rhosadu@ncsu.edu>
2026-01-05 16:09:50 +0530

    Maintaining stats for tests

--- tests/test_server.rs
+++ tests/test_server.rs
@@ -2,15 +2,16 @@
 mod tests {
     use regex::Regex;
     use reqwest::{Client, StatusCode};
-    use tokio::time::{Duration, sleep};
+    use tokio::time::{Duration, Instant, sleep};
 
     use std::{
+        collections::HashMap,
         fs::File,
-        io::{BufRead, BufReader},
+        io::{BufRead, BufReader, BufWriter, Write},
         path::Path,
     };
 
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq, Eq, Hash)]
     enum Method {
         GET,
         PUT,
@@ -24,26 +25,53 @@ mod tests {
         value: String,
     }
 
+    struct RetryResult {
+        response: reqwest::Response,
+        retry_time: u128,
+        retries: u128,
+    }
+
+    struct Stats {
+        success_time: u128,
+        retry_time: u128,
+        success_count: u128,
+        retry_count: u128,
+    }
+
     const MAX_RETRIES: usize = 1000;
     const RETRY_DELAY_MS: u64 = 2000;
 
-    async fn retry<F, Fut>(mut f: F) -> Result<reqwest::Response, reqwest::Error>
+    async fn retry<F, Fut>(mut f: F) -> Result<RetryResult, reqwest::Error>
     where
         F: FnMut() -> Fut,
         Fut: std::future::Future<Output = Result<reqwest::Response, reqwest::Error>>,
     {
         let mut last_resp = None;
+        let mut retry_time = 0;
+        let mut retries = 0;
 
         for attempt in 0..MAX_RETRIES {
+            let start = Instant::now();
             match f().await {
                 Ok(resp) => {
+                    let elapsed = start.elapsed().as_micros();
                     if resp.status().is_success() || resp.status() == StatusCode::NOT_FOUND {
-                        return Ok(resp);
+                        return Ok(RetryResult {
+                            response: resp,
+                            retry_time: retry_time,
+                            retries: retries,
+                        });
                     }
+
+                    retry_time += elapsed;
+                    retries += 1;
 
                     last_resp = Some(resp);
                 }
                 Err(e) => {
+                    let elapsed = start.elapsed().as_micros();
+                    retry_time += elapsed;
+                    retries += 1;
                     if attempt + 1 == MAX_RETRIES {
                         return Err(e);
                     }
@@ -55,13 +83,45 @@ mod tests {
             }
         }
 
-        Ok(last_resp.expect("retry exhausted without response"))
+        Ok(RetryResult {
+            response: last_resp.expect("retry exhausted without response"),
+            retry_time,
+            retries,
+        })
     }
 
     #[tokio::test]
     async fn test_server() {
         let test_file_path = Path::new("tests/put-delete.txt");
         let path = test_file_path.display();
+        let mut stats = HashMap::<Method, Stats>::new();
+        stats.insert(
+            Method::GET,
+            Stats {
+                success_time: 0,
+                retry_time: 0,
+                success_count: 0,
+                retry_count: 0,
+            },
+        );
+        stats.insert(
+            Method::PUT,
+            Stats {
+                success_time: 0,
+                retry_time: 0,
+                success_count: 0,
+                retry_count: 0,
+            },
+        );
+        stats.insert(
+            Method::DELETE,
+            Stats {
+                success_time: 0,
+                retry_time: 0,
+                success_count: 0,
+                retry_count: 0,
+            },
+        );
         let file = match File::open(test_file_path) {
             Err(err) => panic!("Unable to open file {}:{}", path, err),
             Ok(file) => file,
@@ -117,53 +177,101 @@ mod tests {
                 }
             })
             .collect();
+
         for req in requests {
             match req.method {
                 Method::GET => {
-                    let resp = retry(|| {
+                    let start = Instant::now();
+                    let result = retry(|| {
                         client
                             .get(format!("http://localhost:8000/{}", req.key))
                             .send()
                     })
-                    .await;
+                    .await
+                    .unwrap();
+                    let success_time = start.elapsed().as_micros() - result.retry_time;
+                    let stat = stats.get_mut(&Method::GET).unwrap();
+                    stat.retry_time += result.retry_time;
+                    stat.retry_count += result.retries;
+                    stat.success_time += success_time;
+                    stat.success_count += 1;
+
+                    let response = result.response;
                     match req.value.is_empty() {
-                        true => {
-                            assert_eq!(resp.is_ok(), true);
-                            let response = resp.unwrap();
-                            assert_eq!(response.status(), StatusCode::NOT_FOUND);
-                        }
+                        true => assert_eq!(response.status(), StatusCode::NOT_FOUND),
                         false => {
-                            assert_eq!(resp.is_ok(), true);
-                            let response = resp.unwrap();
                             assert_eq!(response.status(), StatusCode::OK);
-                            let response_body = response.text().await.unwrap();
-                            assert_eq!(response_body, req.value);
+                            assert_eq!(response.text().await.unwrap(), req.value);
                         }
                     }
                 }
                 Method::PUT => {
-                    let resp = retry(|| {
+                    let start = Instant::now();
+                    let result = retry(|| {
                         client
                             .put(format!("http://localhost:8000/{}", req.key))
                             .body(req.value.clone())
                             .send()
                     })
-                    .await;
-                    assert_eq!(resp.is_ok(), true);
-                    assert_eq!(resp.unwrap().status(), StatusCode::OK);
+                    .await
+                    .unwrap();
+                    let success_time = start.elapsed().as_micros() - result.retry_time;
+                    let stat = stats.get_mut(&Method::PUT).unwrap();
+                    stat.retry_time += result.retry_time;
+                    stat.retry_count += result.retries;
+                    stat.success_time += success_time;
+                    stat.success_count += 1;
+
+                    let resp: reqwest::Response = result.response;
+                    assert_eq!(resp.status(), StatusCode::OK);
                 }
                 Method::DELETE => {
-                    let resp = retry(|| {
+                    let start = Instant::now();
+                    let result = retry(|| {
                         client
                             .delete(format!("http://localhost:8000/{}", req.key))
                             .send()
                     })
-                    .await;
-                    assert_eq!(resp.is_ok(), true);
-                    assert_eq!(resp.unwrap().status(), StatusCode::OK);
+                    .await
+                    .unwrap();
+                    let success_time = start.elapsed().as_micros() - result.retry_time;
+                    let stat = stats.get_mut(&Method::DELETE).unwrap();
+                    stat.retry_time += result.retry_time;
+                    stat.retry_count += result.retries;
+                    stat.success_time += success_time;
+                    stat.success_count += 1;
+
+                    let resp: reqwest::Response = result.response;
+                    assert_eq!(resp.status(), StatusCode::OK);
                 }
                 Method::INVALID => println!("Skipping req"),
             };
+        }
+
+        let mut writer = BufWriter::new(File::create("stats.log").unwrap());
+
+        for (method, stat) in &stats {
+            writer
+                .write_all(
+                    format!(
+                        "{:?}: success_count={}, retries={}, success_avg={}µs, retry_avg={}µs\n",
+                        method,
+                        stat.success_count,
+                        stat.retry_count,
+                        if stat.success_count > 0 {
+                            stat.success_time / stat.success_count
+                        } else {
+                            0
+                        },
+                        if stat.retry_count > 0 {
+                            stat.retry_time / stat.retry_count
+                        } else {
+                            0
+                        },
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
         }
     }
 }
